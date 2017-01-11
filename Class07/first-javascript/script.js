2f08e09013ed574b7a5fd1bd7a73d7868b2ab7a1
rachkim214 <RachelK@Rachels-MacBook-Pro.local>
2017-01-10 21:32:20 -0500

    Class work 07

---
+++ Class07/first-javascript/script.js
@@ -0,0 +1,22 @@
+// alert('This is an alert. This is annoying');
+
+document.write('Writing to the document!');
+
+var firstNum = 10*10;
+var myName = "My name is Jonas";
+
+document.write(firstNum);
+document.write(myName);
+
+function stupidAlert(){
+	alert('stupid');
+	alert('alerts are stupid');
+}
+
+document.getElementById('title').style.color='red';
+
+// document.write(10*10);
+// document.write(100/10);
+// document.write(50-17);
+// document.write(50+17);
+// document.write('Rachel Kim');
